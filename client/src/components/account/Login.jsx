6d5fecd82b91d74f844fcb872bc9f8419e20087f
Jaycee-9 <thakkurjay2@gmail.com>
2024-06-11 00:05:33 +0530

    add loader during user login

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -3,18 +3,23 @@ import { useState } from "react";
 import { userLogin } from "@/service/api";
 import { useRouter } from "next/navigation";
 import { useData } from "@/utils/context";
-const intialValues = {
+import CircularProgress from "@mui/material/CircularProgress";
+
+const initialValues = {
   email: "",
   password: "",
 };
+
 function LoginForm() {
   const router = useRouter();
-  const [formFields, setFormFields] = useState(intialValues);
+  const [formFields, setFormFields] = useState(initialValues);
   const [message, setMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { setUser } = useData();
+
   const loginUser = async (evt) => {
     evt.preventDefault();
-
+    setLoading(true);
     try {
       const res = await userLogin(formFields);
       setUser(res.data);
@@ -22,7 +27,9 @@ function LoginForm() {
       setMessage(null);
       router.push("/");
     } catch (error) {
-      setMessage(error.response.data.msg);
+      setMessage(error.response?.data?.msg);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -32,31 +39,40 @@ function LoginForm() {
 
   return (
     <div className="max-w-[400px] h-[400px] mx-auto">
-      <form onSubmit={loginUser} className=" p-1 mt-[20%]">
-        <input
-          type="email"
-          name="email"
-          value={formFields.email}
-          onChange={handleInput}
-          placeholder="E-mail"
-          className="w-full max-w-[90%] sm:max-w-[380px] border-b-2 border-black mx-auto h-12 my-2 px-4 text-lg sm:text-2xl focus:outline-none"
-          required
-        />
-
-        <input
-          type="password"
-          name="password"
-          value={formFields.password}
-          onChange={handleInput}
-          placeholder="Password"
-          className="w-full max-w-[90%] sm:max-w-[380px] border-b-2 border-black mx-auto h-12 my-2 px-4 text-lg sm:text-2xl focus:outline-none"
-          required
-        />
-        {message === null ? "" : <h1 className="mt-4 h-5">{message}</h1>}
-        <button className="w-full px-8 py-3 bg-gradient-to-r mt-[50px] from-green-400 to-blue-500 text-white font-bold rounded-full shadow-lg hover:from-green-500 hover:to-blue-600 focus:outline-none focus:ring-4 focus:ring-green-300 focus:ring-opacity-50 transition transform hover:-translate-y-1 hover:scale-105">
-          Login
-        </button>
-      </form>
+      {loading ? (
+        <div className="max-w-[20px] mt-[100px] mx-auto">
+          <CircularProgress />
+        </div>
+      ) : (
+        <form onSubmit={loginUser} className="p-1 mt-[20%]">
+          <input
+            type="email"
+            name="email"
+            value={formFields.email}
+            onChange={handleInput}
+            placeholder="E-mail"
+            className="w-full max-w-[90%] sm:max-w-[380px] border-b-2 border-black mx-auto h-12 my-2 px-4 text-lg sm:text-2xl focus:outline-none"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            value={formFields.password}
+            onChange={handleInput}
+            placeholder="Password"
+            className="w-full max-w-[90%] sm:max-w-[380px] border-b-2 border-black mx-auto h-12 my-2 px-4 text-lg sm:text-2xl focus:outline-none"
+            required
+          />
+          {message && <h1 className="mt-4 h-5">{message}</h1>}
+          <button
+            type="submit"
+            className="w-full px-8 py-3 bg-gradient-to-r mt-[50px] from-green-400 to-blue-500 text-white font-bold rounded-full shadow-lg hover:from-green-500 hover:to-blue-600 focus:outline-none focus:ring-4 focus:ring-green-300 focus:ring-opacity-50 transition transform hover:-translate-y-1 hover:scale-105"
+            disabled={loading}
+          >
+            Login
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import CircularProgress from "@mui/material/CircularProgress";
 import { useData } from "@/utils/context";
