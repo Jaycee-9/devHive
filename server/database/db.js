d85ce5132eab06cf5b1e9e3b362daf13bdafd43a
Jaycee-9 <thakkurjay2@gmail.com>
2024-06-10 14:28:25 +0530

    db file change

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const connectToDb = async () => {
-  await mongoose.connect(process.env.connectionString);
+  await mongoose.connect(
+    "mongodb+srv://devHive:PZXYee24RSALv1c7@cluster0.lcqoswa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
   console.log("database connected");
 };
 
