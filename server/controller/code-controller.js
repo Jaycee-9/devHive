2aa7b6856ddf6f1231f22b1373cd6daa4a6ebe10
Jaycee-9 <thakkurjay2@gmail.com>
2024-06-15 17:09:27 +0530

    set up backend route for code upload

---
+++ server/controller/code-controller.js
@@ -0,0 +1,4 @@
+export const uploadCode = (req, res) => {
+  console.log(req.body);
+  return res.status(200).json({ msg: "mil gya" });
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userSignup, userLogin } from "../controller/user-controller.js";
+import { uploadCode } from "../controller/code-controller.js";
 
 const router = Router();
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
+router.post("/add_code", uploadCode);
 
 export default router;
