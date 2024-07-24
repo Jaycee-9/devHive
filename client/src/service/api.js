a1f2f4b9aa786f92ea282f60c08277f71f480dc2
Jaycee-9 <thakkurjay2@gmail.com>
2024-07-24 14:33:18 +0530

    change url for deployment

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://dev-hive-server.vercel.app";
+const URL = "https://dev-hive-server.vercel.app/";
 
 export const userSignup = async (credentials) => {
   try {
