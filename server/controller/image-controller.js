8e4e9696f45071f491482da3ae670679c88679a1
Jaycee-9 <thakkurjay2@gmail.com>
2024-07-24 14:33:06 +0530

    url fixes

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from "gridfs-stream";
 import mongoose from "mongoose";
 
-const url = "https://dev-hive-server.vercel.app";
+const url = "https://dev-hive-server.vercel.app/";
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
